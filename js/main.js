470e0f616fa549531112e4c47ea43c3742e681a9
kurskii@ <kurskii@>
2021-06-28 20:28:56 +0200

    fixes 17jun

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ function loadCase(caseToLoad) {
     $('.desktop-content .containery').append('<div class="new-html inactive"></div>');
     $(".desktop-content .containery .new-html").load(`${caseToLoad}.html .case-study-html`, function (responseText, textStatus, XMLHttpRequest) {
         if (textStatus === "success") {
-            $('head').append('<link rel="stylesheet" type="text/css" href="css/cases.css?v=8">');
+            $('head').append('<link rel="stylesheet" type="text/css" href="css/cases.css?v=9">');
             $('link[title="insightCss"]').remove();
             loadImage($(".case-hero-img:first-child").attr('src')).then(() => {
                 console.log('img loaded');
